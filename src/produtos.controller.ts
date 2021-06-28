9b13f363c1fd57121ac2371a28c814a429ddd0fa
tiagoernandes <tiagoestevam7@hotmail.com>
2021-06-27 21:45:38 -0300

    moc Produtos

--- src/produtos.controller.ts
+++ src/produtos.controller.ts
@@ -1,29 +1,39 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Produto } from './produto.model';
 
 @Controller('produtos')
 export class ProdutosController {
+
+  produtos: Produto[] = [
+    new Produto("LVL01","Javascript",29.50),
+    new Produto("LVL02","Java",50),
+    new Produto("LVL02","C#",40),
+    new Produto("LVL02","PHP",20)
+  ];
+
     @Get()
-    obterTodos(): string {
-      return 'Lista Todos os Produtos';
+    obterTodos(): Produto[] {
+      return this.produtos;
     }
 
     @Get(':id')
-    obterUm(@Param() params): string {
-      return `Retorna os dados do produto ${params.id}`
+    obterUm(@Param() params): Produto {
+      return this.produtos[params.id]
     }
 
     @Post()
-    criar(@Body() produto):string {
-      console.log(produto)
+    criar(@Body() produto:Produto):string {
+     this.produtos.push(produto)
       return `Produto criado com sucesso`
     }
     @Put(':id')
-    alterar(@Body() produto,@Param() params): string {
-      console.log(produto);
-      return `Produto Alterado id ${params.id}`
+    alterar(@Body() produto:Produto,@Param() params):Produto{
+     this.produtos[params] = {...produto}
+      return  this.produtos[params]
     }
     @Delete(':id')
     apagar(@Param() params):string {
+      this.produtos.pop();
       return `Produto ${params.id} apagado com sucesso`
     }
 }
